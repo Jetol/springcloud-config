465a1b0af0b39f3e715711e7c9338c45b447b4a9
Administrator <375978142@qq.com>
2019-04-24 14:35:51 +0800

    springcloud配置中心使用eureka服务注册方式。 问题一：服务注册中心没有发现config-client注册到eureka。 问题二：使用自己的配置问价不行，换作者的配置文件就能访问到。

--- config-client/src/main/java/com/xieluyang/configclient/action/ConfigClientController.java
+++ config-client/src/main/java/com/xieluyang/configclient/action/ConfigClientController.java
@@ -1,10 +1,11 @@
 package com.xieluyang.configclient.action;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RefreshScope
+@RefreshScope
 @RestController
 public class ConfigClientController {
 
